71d6d81a3c23fdbbb7d6db75c332ebfe737ceba6
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-01-07 17:16:00 +0200

    Add util method: isIn

--- src/main/java/org/monroe/team/android/box/Lists.java
+++ src/main/java/org/monroe/team/android/box/Lists.java
@@ -1,7 +1,7 @@
 package org.monroe.team.android.box;
 
 import org.monroe.team.android.box.db.DAOSupport;
-
+import android.util.Pair;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -35,6 +35,13 @@ public class Lists {
     }
 
     public static <ElemType> ElemType getLast(List<ElemType> list) {
-        return list.get(list.size()-1);
+        return list.get(list.size() - 1);
+    }
+
+    public static <Type1,Type2>  boolean in(Collection<Type1> collection, Type2 item, Closure<Pair<Type1,Type2>,Boolean> compare) {
+        for (Type1 type1 : collection) {
+            if (compare.execute(new Pair<Type1, Type2>(type1,item))) return true;
+        }
+        return false;
     }
 }
